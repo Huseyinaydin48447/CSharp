98b841157e91dfcac66dfe2aa96d4d4605f97bc6
Hüseyin Aydın <109576958+Huseyinaydin48447@users.noreply.github.com>
2025-08-19 12:07:15 +0300

    C# WinForms Ürün Yönetimi Uygulaması (Dapper + SQL Server)
    Bu proje, C# WinForms ile geliştirilmiş bir ürün yönetim uygulamasıdır. SQL Server veritabanıyla entegre çalışan uygulama, Dapper mikro ORM kütüphanesi kullanılarak oluşturulmuştur. Temel CRUD (Create, Read, Update, Delete) işlemlerini içermektedir.
    
    ✨ Özellikler
    Ürün ekleme, silme, güncelleme ve listeleme işlemleri
    
    Dapper ile veritabanı işlemleri
    
    SQL Server Management Studio entegrasyonu
    
    Temel form validasyonları
    
    Kategori bazlı ürün yönetimi
    
    🔧 Teknoloji Yığını
    C# WinForms
    
    Dapper ORM
    
    SQL Server
    
    .NET Framework/.NET Core

---
+++ CSharpEğitim501/CSharpEğitim501/ConnectionString/DatabaseConnectionString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpEğitim501.ConnectionString
+{
+    public static class DatabaseConnectionString
+    {
+        public static void SQLDatabaseConnectionString()
+        {
+            SqlConnection connection = new SqlConnection(
+                    "Server = DESKTOP-6BI1NKL; initial Catalog = EgitimKampi501Db; integrated Security = true;");
+        }
+    }
+}
---
+++ CSharpEğitim501/CSharpEğitim501/Form1.cs
@@ -0,0 +1,42 @@
+﻿using CSharpEğitim501.Dtos;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharpEğitim501
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        SqlConnection connection = new SqlConnection(
+      "Server=(local); Database=EgitimKampi501Db; Trusted_Connection=True;");
+        private async void btnList_Click(object sender, EventArgs e)
+        {
+            string query = "Select * From TblProduct";
+            var values=await connection.QueryAsync<ResultProductDto>(query);
+            dataGridView1.DataSource = values;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
